1b60f4108cb790a797115eca20a8f738bdecfa2b
Carl <carl@kratzundraabe.de>
2021-04-08 10:44:37 +0200

    removed test.js

--- src/test.js
+++
@@ -1,8 +0,0 @@
-function getExtention(filename) {
-    console.log(filename.split('.'));
-    return filename.split('.').slice(1).join('.').toLowerCase();
-}
-
-console.log(getExtention('index.html'));
-console.log(getExtention('index.html.test'));
-console.log(getExtention('index'));
