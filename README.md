73a18c4174f8a563c6aab366d75ca6fc1e75d587
Carl <carl@kratzundraabe.de>
2021-04-09 02:42:57 +0200

    Changes to moveUndetectedFilesToFolder

--- README.md
+++ README.md
@@ -23,4 +23,7 @@ node sort-folder.js [Path to directory]
 
 # Config
 1. Open config.json
-2. In the "folder" object add a key (e.g. test) and the fitting file extentions (e.g. ["jpg"]) 
+2. In the "folder" object add a key (e.g. test) and the fitting file extentions (e.g. ["jpg"])
+
+## Important information
+At macOS the programs like "test.app" are actually *not* treated as a file. Apps are "Package Bundles" just like a any other Unix directory but treated differently by the OS. When sorting, the script first checks whether the object is a file or a folder. If the object is a folder, the script does not check the extension of the file. Adding .app to the config.json would therefore be pointless.
--- src/sort-folder.js
+++ src/sort-folder.js
@@ -1,12 +1,17 @@
 const config = require('./config.json');
-const fs = require('fs');
-const path = require('path');
 const {
-    inherits
-} = require('util');
+    existsSync,
+    lstatSync,
+    readdir,
+    mkdir,
+    renameSync
+} = require('fs');
 const {
-    join
+    join,
+    extname
 } = require('path');
+
+var folderList = new Array();
 
 var args = process.argv.slice(2);
 if (args.length === 0) {
@@ -15,8 +20,8 @@ if (args.length === 0) {
 } else {
     var fldrPath = args[0];
 
-    if (fs.existsSync(fldrPath)) {
-        if (!fs.lstatSync(fldrPath).isDirectory()) {
+    if (existsSync(fldrPath)) {
+        if (!lstatSync(fldrPath).isDirectory()) {
             console.log('Path is not a directory.');
             process.exitCode = 1;
         } else {
@@ -24,10 +29,11 @@ if (args.length === 0) {
             const configFldr = Object.keys(config.folder);
             for (var i = 0; i < configFldr.length; i++) {
                 const attr = configFldr[i];
-                if (fs.existsSync(path.join(fldrPath, attr))) {
+                folderList.push(attr);
+                if (existsSync(join(fldrPath, attr))) {
                     console.log('\x1b[35m%s\x1b[0m', `Folder '${attr}' exists at: '${fldrPath}'`);
                 } else {
-                    fs.mkdir(path.join(fldrPath, attr), () => {
+                    mkdir(join(fldrPath, attr), () => {
                         console.log('\x1b[32m%s\x1b[0m', `Folder '${attr}' created at: '${fldrPath}'`);
                     });
                 }
@@ -43,28 +49,37 @@ if (args.length === 0) {
 }
 
 function moveFiles(targetFldr) {
-    fs.readdir(targetFldr, (err, files) => {
+    readdir(targetFldr, (err, files) => {
         if (err) {
             console.log(err);
             process.exitCode = 1;
         }
         for (var i = 0; i < files.length; i++) {
-            const filePath = path.join(targetFldr, files[i])
-            if (fs.lstatSync(filePath).isFile()) {
-                const suffix = path.extname(files[i]).slice('1').toLowerCase();
+            const filePath = join(targetFldr, files[i])
+            if (lstatSync(filePath).isFile()) {
+                const suffix = extname(files[i]).slice('1').toLowerCase();
                 for (var a = 0; a < Object.keys(config.folder).length; a++) {
                     if (config.folder[Object.keys(config.folder)[a]].includes(suffix)) {
-                        console.log(true, files[i])
-                        fs.renameSync(filePath, path.join(targetFldr, Object.keys(config.folder)[a], files[i]));
+                        renameSync(filePath, join(targetFldr, Object.keys(config.folder)[a], files[i]));
                         break;
                     } else {
                         if (a === Object.keys(config.folder).length - 1) {
                             if (config.moveUndetectedFilesToFolder.enable && !config.moveUndetectedFilesToFolder.blacklist.includes(suffix)) {
-                                fs.renameSync(filePath, path.join(targetFldr, config.moveUndetectedFilesToFolder.nameOfFolder, files[i]));
+                                renameSync(filePath, join(targetFldr, config.moveUndetectedFilesToFolder.nameOfFolder, files[i]));
                             }
                         }
                     }
-                } 
+                }
+            } else if (lstatSync(filePath).isDirectory()) {
+                if (config.moveUndetectedFilesToFolder.enable) {
+                    if (config.moveUndetectedFilesToFolder.moveFolders) {
+                        if (!(folderList.includes(files[i]))) {
+                            if (!(files[i] === config.moveUndetectedFilesToFolder.nameOfFolder)) {
+                                renameSync(filePath, join(targetFldr, config.moveUndetectedFilesToFolder.nameOfFolder, files[i]));
+                            }
+                        }
+                    }
+                }
             }
             if (i === files.length - 1) {
                 console.log('\x1b[34m%s\x1b[0m', 'Operation finished');
@@ -76,10 +91,10 @@ function moveFiles(targetFldr) {
 function initOthersFolder() {
     const undetectedFldrName = config.moveUndetectedFilesToFolder.nameOfFolder;
     if (config.moveUndetectedFilesToFolder.enable) {
-        if (fs.existsSync(path.join(fldrPath, undetectedFldrName))) {
+        if (existsSync(join(fldrPath, undetectedFldrName))) {
             console.log('\x1b[35m%s\x1b[0m', `Folder '${undetectedFldrName}' exists at: '${fldrPath}'`);
         } else {
-            fs.mkdir(path.join(fldrPath,undetectedFldrName), () => {
+            mkdir(join(fldrPath, undetectedFldrName), () => {
                 console.log('\x1b[32m%s\x1b[0m', `Folder '${undetectedFldrName}' created at: '${fldrPath}'`);
             });
         }
